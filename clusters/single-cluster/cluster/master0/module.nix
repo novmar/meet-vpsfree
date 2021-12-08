cf57c55d41a6cec3fe318311caa96d8ad18cd695
Marcel Novotny <marceln@xcroco.com>
2021-12-08 20:04:13 +0100

    switched on new host

--- clusters/single-cluster/cluster/master0/module.nix
+++ clusters/single-cluster/cluster/master0/module.nix
@@ -2,9 +2,10 @@
 {
   cluster."master0" = {
     spin = "nixos";
-    swpins.channels = [ "nixos-unstable" "vpsadminos-master" ];
+    swpins.channels = [ "nixos-stable" "vpsadminos-master" ];
         host = {
-        target = "37.205.14.30";
+#        target = "37.205.14.30";
+        target = "37.205.14.16";
   };
   };
 }
